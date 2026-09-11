d8c7501a47ab7f55a13673cbe5774d027785b56e
dragonMaster1748 <34729983+dragonMaster1748@users.noreply.github.com>
2026-09-11 13:33:22 -0700

    Update settings for overlay mode

--- src/settings.ts
+++ src/settings.ts
@@ -2,12 +2,10 @@ import { App, PluginSettingTab, Setting } from "obsidian";
 import LapelPlugin from "./index";
 
 export interface LapelSettings {
-  showBeforeLineNumbers: boolean;
   showInSourceMode: boolean;
 }
 
 export const DEFAULT_SETTINGS: LapelSettings = {
-  showBeforeLineNumbers: true,
   showInSourceMode: false,
 };
 
@@ -21,36 +19,16 @@ export class LapelSettingsTab extends PluginSettingTab {
 
   display(): void {
     const { containerEl } = this;
-
     containerEl.empty();
 
     new Setting(containerEl)
-      .setName("Show before line numbers")
-      .setDesc(
-        "Toggle whether the heading markers are shown before or after the line numbers in the gutter."
-      )
-      .addToggle((toggle) =>
-        toggle
-          .setValue(this.plugin.settings.showBeforeLineNumbers)
-          .onChange(async (value) => {
-            void this.plugin.updateSettings(() => ({
-              showBeforeLineNumbers: value,
-            }));
-          })
-      );
-
-    new Setting(containerEl)
       .setName("Show in source mode")
-      .setDesc(
-        "Toggle whether the heading markers are shown in in source mode."
-      )
+      .setDesc("Toggle whether the overlay heading markers are shown in source mode.")
       .addToggle((toggle) =>
         toggle
           .setValue(this.plugin.settings.showInSourceMode)
           .onChange(async (value) => {
-            void this.plugin.updateSettings(() => ({
-              showInSourceMode: value,
-            }));
+            void this.plugin.updateSettings(() => ({ showInSourceMode: value }));
           })
       );
   }
